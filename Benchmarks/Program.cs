2eb10b2ddbde81cbeeb9da490eb284867878a69a
Sinitsyn Pavel <pgsinitsyn@gmail.com>
2018-05-20 17:10:44 +0200

    misc

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BenchmarkDotNet.Reports;
-using BenchmarkDotNet.Running;
-
-namespace Benchmarks
+﻿namespace Benchmarks
 {
-    
-
     class Program
     {
         static void Main(string[] args)
